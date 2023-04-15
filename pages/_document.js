f6c243c8d1c3de0a93939e629aa737607da46fbe
Abhiavati20 <abhishekaa1112@gmail.com>
2023-04-15 22:20:17 +0530

    Greenvest frontend

--- pages/_document.js
+++ pages/_document.js
@@ -4,7 +4,9 @@ export default function Document() {
   return (
       <Html lang="en">
           <Head>
+            <title>GreenVest</title>
             <link href='https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap' rel="stylesheet" />
+            <link rel = "icon" type = "image/png"  href = "/images/Logo.png" />
           </Head>
           <body>
               <Main />
