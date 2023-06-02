57203cc4b13221d05ba59f98267dcc720f85530c
Abhiavati20 <abhishekaa1112@gmail.com>
2023-06-02 12:43:16 +0530

    Updating content changes

--- pages/services/index.js
+++ pages/services/index.js
@@ -27,10 +27,11 @@ const services = () => {
                     <Image src="/images/service1.png" alt="service 1" width={600} height={450} />
                 </div>
                 <div className='flex flex-col'>
-                    <h2 className='text-xl font-semibold my-4'>Enterprise <br/> (large, medium, small)</h2>
-                    <p className='text-base text-[#6B6B6B] my-2'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, .
-                        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-                        Ut enim ad minim veniam, quis nostrud exercitation ullamco.
+                    <h2 className='text-xl font-semibold my-4'>Enterprise </h2>
+                    <p className='text-base text-[#6B6B6B] my-2'>
+                        All enterprises be it large, medium and small, committed to Net 0 for a sustainable business, can start with offsetting their power consumptions with our curated and tokenised RECs.
+                        <br/>
+                        Businesses based on sustainability goals have proven to optimize costs and enable better management of limited resources. Earn points towards LEED certification and reduce the environmental impact of buildings.
                     </p>
                     <button className='px-6 py-2 text-white my-2 rounded-lg bg-[#04A6E7] w-[20%]' onClick={() => router.push('/services/enterprise')}>Check</button>
                 </div>
@@ -42,10 +43,11 @@ const services = () => {
                     <Image src="/images/service1.png" alt="service 1" width={600} height={450} />
                 </div>
                 <div className='flex flex-col'>
-                    <h2 className='text-xl font-semibold my-4'>Commercial <br/> Property Developer</h2>
-                    <p className='text-base text-[#6B6B6B] my-2'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, .
-                        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-                        Ut enim ad minim veniam, quis nostrud exercitation ullamco.
+                    <h2 className='text-xl font-semibold my-4'>Developers</h2>
+                    <p className='text-base text-[#6B6B6B] my-2'>Irrespective of the scale of the asset, every building has an immense opportunity to upgrade its value for higher returns. You can start by offsetting your power consumption with our curated and tokenised RECs.
+                    <br/>
+                    Tokere allows you to view, track, consume and retire your RECs in real time. Enterprises can export reports for REC purchases, consumption and retirement to prove their initiatives towards Net 0
+
                     </p>
                     <button className='px-6 py-2 text-white my-2 rounded-lg bg-[#04A6E7] w-[20%]' onClick={() => router.push('/services/developer')}>Check</button>
                 </div>
@@ -56,10 +58,10 @@ const services = () => {
                     <Image src="/images/service2.png" alt="service 1" width={1980} height={990} />
                 </div>
                 <div className='flex flex-col'>
-                    <h2 className='text-xl font-semibold my-4'>Residential <br/> (Gated, RWAs, societies)</h2>
-                    <p className='text-base text-[#6B6B6B] my-2'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, .
-                        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-                        Ut enim ad minim veniam, quis nostrud exercitation ullamco.
+                    <h2 className='text-xl font-semibold my-4'>Individuals</h2>
+                    <p className='text-base text-[#6B6B6B] my-2'>Discerning, globally conscious and trendsetting individuals also how have an opportunity to contribute to global Net 0. Connect with us to offset your power consumption with our curated and tokenised RECs. Contribute to the UN led sustainable development goals (SDGs).
+                    <br/>
+                    Document consumption of renewables and reliably claim GHG emission reductions, Trace the origin of electricity and chosen technology, such as solar, wind, hydro, geothermal or bioenergy Align with GHG Protocol Scope 2 guidance, which are voluntary corporate environmental standards.
                     </p>
                     <button className='px-6 py-2 text-white my-2 rounded-lg bg-[#04A6E7] w-[10%]' onClick={() => router.push('/services/residential')}>Check</button>
                 </div>
