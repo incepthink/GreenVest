901eb985edfecc8ebb01b00c4a1555a47be96319
Abhiavati20 <abhishekaa1112@gmail.com>
2023-06-12 18:23:27 +0530

    updated

--- components/Header/Header.jsx
+++ components/Header/Header.jsx
@@ -8,7 +8,7 @@ const Header = () => {
     const { state, dispatch } = useContext(StoreContext);
 
     return (
-        <div className="max-w-screen md:h-[50%] h-[90vh] bg-[#04a6e71a] md:rounded-bl-[163px] rounded-bl-[80px]">
+        <div className="max-w-screen md:h-[50%] h-screen bg-[#04a6e71a] md:rounded-bl-[163px] rounded-bl-[80px]">
             <div className="flex flex-col md:flex-row justify-around items-center">
                 <div className="md:w-[55%] w-[100%] md:px-20 px-8 md:py-24 py-8 flex-col font-bebas uppercase">
                     <h1 className="md:text-7xl text-5xl flex items-center">
