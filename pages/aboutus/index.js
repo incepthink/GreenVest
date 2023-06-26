9b368f6ae5a7e0194bbbad203b8505ffd3f19582
Abhiavati20 <abhishekaa1112@gmail.com>
2023-06-26 11:25:11 +0530

    fixing some paddings

--- pages/aboutus/index.js
+++ pages/aboutus/index.js
@@ -28,8 +28,8 @@ export default function Home() {
           </h1> */}
           <h2 className="text-center text-4xl font-bold my-6 font-poppins"> Why Choose Us ?</h2>
           <div className="md:w-[90%] w-[95%]  flex flex-col md:flex-row mx-auto justify-between items-center my-4">
-            <Image src="/images/flowChart3.png" alt="vision 1" className="shadow-md shadow-[#04A6E7]" width={600} height={600} style={{"objectFit":"contain", }} />
-            <Image src="/images/Website.jpg" alt="vision 1" className="shadow-md shadow-[#04A6E7]" width={600} height={600} style={{"objectFit":"contain", }} />
+            <Image src="/images/flowChart3.png" alt="vision 1" className="shadow-md my-4 shadow-[#04A6E7]" width={600} height={600} style={{"objectFit":"contain", }} />
+            <Image src="/images/Website.jpg" alt="vision 1" className="shadow-md my-4 shadow-[#04A6E7]" width={600} height={600} style={{"objectFit":"contain", }} />
           </div>
           <div className="max-w-screen md:h-[60vh] h-[50vh] bg-home-background-1 bg-cover " />
           <MeetOurTeam />
