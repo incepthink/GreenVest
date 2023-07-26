20e9f72743cc55dca7613f017b027bbecdd6e0ce
Abhiavati20 <abhishekaa1112@gmail.com>
2023-07-26 15:41:35 +0530

    deleted some unwanted components

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,8 @@
-import Ecosystem from "@/components/Ecosystem/Ecosystem";
 import Footer from "@/components/Footer/Footer";
 import Header from "@/components/Header/Header";
 import MeetOurTeam from "@/components/MeetOurTeam/MeetOurTeam";
 import OurPartners from "@/components/OurPartners/index";
 import Navbar from "@/components/Navbar/Navbar";
-import RecentProjects from "@/components/RecentProjects/RecentProjects";
 import Services from "@/components/Services/Services";
 import Visions from "@/components/Visions/Visions";
 import Head from "next/head";
@@ -19,13 +17,8 @@ export default function Home() {
           <Navbar />
           <Header />
           <Visions />
-          {/* <Mission /> */}
           <Services />
-          {/* <Ecosystem /> */}
-          {/* <h1 className="text-3xl font-bold text-center underline">
-              Hello World!!
-
-          </h1> */}
+          
           <h2 className="text-center text-3xl font-bold mt-6 font-poppins"> This is how we work </h2>
           <div className="md:w-[90%] w-[95%] font-poppins flex flex-col md:flex-row mx-auto justify-between items-center ">
             <Image src="/images/workflow1.png" alt="vision 1" className="shadow-md my-4 mx-auto shadow-[#04A6E7] cursor-pointer hover:scale-105 hover:duration-500" width={700} height={700}  />
