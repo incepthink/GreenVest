6763475cb2355ab15fea906600c63fd2285076c1
Rajesh <rajesh@smrtpl.com>
2024-05-24 09:56:22 +0530

    Redirect try 01

--- next.config.js
+++ next.config.js
@@ -9,4 +9,15 @@ const nextConfig = {
   }
 }
 
-module.exports = nextConfig
+// module.exports = nextConfig
+module.exports = {
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: 'https://tokere.com',
+        permanent: true,
+      },
+    ]
+  },
+}
